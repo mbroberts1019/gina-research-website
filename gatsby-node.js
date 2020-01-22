6f4b7cea08f0e8cbaf54db1986de9edc2c46ef1f
Mike Roberts <mikebroberts@hotmail.com>
2020-01-21 23:38:56 -0700

    fix broken cms

--- gatsby-node.js
+++ gatsby-node.js
@@ -85,3 +85,8 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     })
   }
 }
+
+exports.onCreateDevServer = ({ app }) => {
+  const fsMiddlewareAPI = require('netlify-cms-backend-fs/dist/fs')
+  fsMiddlewareAPI(app)
+}
