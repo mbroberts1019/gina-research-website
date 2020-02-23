45abc105485527dda6dbb1bcaa2e69cfe9061a32
Mike Roberts <46229466+mbroberts1019@users.noreply.github.com>
2020-02-22 21:45:01 -0700

    change footer background (#6)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,9 +10,9 @@ import vimeo from '../img/social/vimeo.svg'
 const Footer = class extends React.Component {
   render() {
     return (
-      <footer className="footer has-background-black has-text-white-ter">
-        <div className="content has-text-centered has-background-black has-text-white-ter">
-          <div className="container has-background-black has-text-white-ter">
+      <footer className="footer has-text-white-ter">
+        <div className="content has-text-centered  has-text-white-ter">
+          <div className="container has-text-white-ter">
             <div className="columns">
               <div className="column is-4">
                 <section className="menu">
