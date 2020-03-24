03aeae1c7d565d5df84330d5aef15b1ad76d33df
Mike Roberts <46229466+mbroberts1019@users.noreply.github.com>
2020-03-23 21:56:17 -0600

    removes social media links and adjusts styles in footer (#4)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,8 +13,7 @@ const Footer = class extends React.Component {
       <footer className="footer has-text-white-ter">
         <div className="content has-text-centered  has-text-white-ter">
           <div className="container has-text-white-ter">
-            <div className="columns">
-              <div className="column is-6">
+
                 <section className="row menu">
                   <ul className="menu-list">
                     <li>
@@ -45,41 +44,7 @@ const Footer = class extends React.Component {
                     
                   </ul>
                 </section>
-              </div>
-              
-              <div className="row is-1 social">
-                <a title="facebook" href="https://facebook.com">
-                  <img
-                    src={facebook}
-                    alt="Facebook"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="twitter" href="https://twitter.com">
-                  <img
-                    className="fas fa-lg"
-                    src={twitter}
-                    alt="Twitter"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="instagram" href="https://instagram.com">
-                  <img
-                    src={instagram}
-                    alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-              </div>
-              
-            </div>
+
             <div className="copywrite">
                 <p>© 2020 mikercodemonster</p>
               </div>
