1fcf75b92942e08fd1dace2fb2fc3fbbdbec9813
Mike Roberts <mikebroberts@hotmail.com>
2021-01-09 12:03:25 -0700

    remove all project files and references

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,13 +1,10 @@
 import CMS from 'netlify-cms-app'
 import uploadcare from 'netlify-cms-media-library-uploadcare'
-
-
 import AboutPagePreview from './preview-templates/AboutPagePreview'
-import ProjectPreview from './preview-templates/ProjectPreview'
 
 CMS.registerMediaLibrary(uploadcare)
 
 
 
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('project', ProjectPreview)
+
