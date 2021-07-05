5115b44fc452eb93674d1b41c21b3158bf6d85fa
Mike Roberts <mikebroberts@hotmail.com>
2021-07-04 22:37:59 -0600

    remove unnec. import, fix off center title in mobile view

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import Layout from '../components/Layout'
 import Bio from '../components/home/Bio'
 import ContactMe from '../components/home/ContactMe'
